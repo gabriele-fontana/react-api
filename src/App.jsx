dca0aa548fcffba1eab94728eb2f83ea03d00ce4
gabriele-fontana <gabrielefontana.2001@gmail.com>
2026-03-17 12:03:41 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState, useEffect } from 'react'
+
+const actressesApi = 'https://lanciweb.github.io/demo/api/actresses/'
+
+function App() {
+
+
+  return 
+
+
+
+
+}
+
+export default App
